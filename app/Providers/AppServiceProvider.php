2bf74afccbe4a3c208d8270639ee159f6e7b332c
Alin M <macoveialinp@gmail.com>
2024-03-21 15:47:24 +0200

    API rate limiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('api', function (Request $request) {
+            // the 2 expressions should be equivalent. 1st one is from laravel 11, 2nd one is from up to laravel 10
+            // return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip()); // https://www.php.net/manual/en/language.operators.comparison.php#language.operators.comparison.ternary "As of PHP 8.0.0, the ternary operator is non-associative."
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip()); // https://laravel.com/docs/11.x/helpers#method-optional
+        });
     }
 }
