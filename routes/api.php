9b113a1e56fbf5d8c4640d86dac39e5f1183acec
Alin M <macoveialinp@gmail.com>
2024-03-22 13:34:28 +0200

    API token authentication - Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,18 @@ use App\Http\Controllers\V1\ImageManipulationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-
-Route::prefix('v1')->group(function() {
-    Route::apiResource('album', AlbumController::class);
-
-    Route::get('image', [ImageManipulationController::class, 'index']);
-    Route::get('image/by-album/{album}', [ImageManipulationController::class, 'getByAlbum']);
-    Route::get('image/{image}', [ImageManipulationController::class, 'show']);
-    Route::post('image/resize', [ImageManipulationController::class, 'resize']);
-    Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::prefix('v1')->group(function() {
+        Route::apiResource('album', AlbumController::class);
+    
+        Route::get('image', [ImageManipulationController::class, 'index']);
+        Route::get('image/by-album/{album}', [ImageManipulationController::class, 'getByAlbum']);
+        Route::get('image/{image}', [ImageManipulationController::class, 'show']);
+        Route::post('image/resize', [ImageManipulationController::class, 'resize']);
+        Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
+    });
 });
